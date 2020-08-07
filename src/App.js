117be0722650d939563f27ef8c39fc7796dfafb9
dedehsantos <dedehfrontend@gmail.com>
2020-08-07 01:53:05 -0300

    fragmentos

--- src/App.js
+++ src/App.js
@@ -3,22 +3,27 @@ import React from 'react';
 import {View, StyleSheet} from 'react-native';
 
 
-import Aleatorio from './componentes/aleatirio'
+
+import Titulo from './componentes/Titulo'
+//import Aleatorio from './componentes/aleatirio'
 /* import Comppadrao, { Comp1, Comp2 } from './componentes/Multi'
 import Primeiro from './componentes/primeiro' */
 //import MinMax from './componentes/MinMax'
 
 export default () =>  (
 <View style={style.App}> 
-   <Aleatorio min={1} max={60}/>
-   <Aleatorio min={1} max={60}/>
-   <Aleatorio min={1} max={60}/>
-   <Aleatorio min={1} max={60}/>
-   <Aleatorio min={1} max={60}/>
-   <Aleatorio min={1} max={60}/>
+  <Titulo principal="Cadastro Produto"
+   segundario="Tela de Cadastro"/>
+   
     
     
     {/*
+    <Aleatorio min={1} max={60}/>
+   <Aleatorio min={1} max={60}/>
+   <Aleatorio min={1} max={60}/>
+   <Aleatorio min={1} max={60}/>
+   <Aleatorio min={1} max={60}/>
+   <Aleatorio min={1} max={60}/>
     <MinMax min={3} max={20}/>
     <MinMax min={1} max={94}/> 
     <Comppadrao/>
---
+++ src/componentes/Titulo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Text} from   'react-native'
+import Estilo from './estilo'
+
+
+export default props =>(
+   <>
+    <Text style={Estilo.fontG}>{props.principal}</Text>
+    <Text>{props.segundario}</Text>
+   </>
+
+)
