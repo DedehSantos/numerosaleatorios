64cf6dd2f961668b1ab8d01f1c0844f12f1543fd
dedehsantos <dedehfrontend@gmail.com>
2020-08-07 02:22:29 -0300

    ainda botao

--- src/componentes/Botao.js
+++ src/componentes/Botao.js
@@ -4,11 +4,23 @@ import {Button} from 'react-native'
 export default props => {
 
     function executar() {
-        console.warn('Exec!!')
+        console.warn('Exec #01 !!!')
     }
      return (
-    <Button 
-        title="Executar"
-        onPress={executar} />
+
+        <>
+       <Button 
+          title="Executar #01"
+          onPress={executar}
+         />
+
+         <Button 
+          title="Executar #02"
+          onPress={function(){
+              console.warn('Exec #02 !!!')
+          }}
+         />
+
+         </>
      )
 }
